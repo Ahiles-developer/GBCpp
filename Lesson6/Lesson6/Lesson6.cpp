81b87ce9577f3d79d38d579062f1ec7f8727a6c4
Ahiles <yakushev_v@rambler.ru>
2022-01-26 14:00:24 +0300

    ДЗ 6

---
+++ Lesson6/Lesson6/Lesson6.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream fout("1.txt");
+
+    if (fout.is_open()) 
+    {
+        fout << "dsffsfas" << 1234213 << endl;
+       
+        fout.close();
+    }
+
+}
+
