81fa8b62e56ae2285df3c4f54f4f412b9ef2f795
Ahiles <yakushev_v@rambler.ru>
2022-01-30 19:45:32 +0300

    ДЗ 7

---
+++ Lesson7/Lesson7_HW/mylib.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+namespace Task_1 {
+	// Инициализация массива
+	void initArray(double array[], int size) {
+		// Заглушка
+	}
+
+	// Печать массива
+	void printArray(const double array[], int size) {
+		for (int i = 0; i < size; i++) {
+			cout << array[i] << " ";
+		}
+	}
+
+	// Подсчет положительных и отрицательных чисел
+	void countNumbers() {
+		// Заглушка
+	}
+}
---
+++ Lesson7/Lesson7_HW/mylib.h
@@ -0,0 +1,6 @@
+#pragma once
+namespace Task_1 {
+	void initArray(double array[], int size);
+	void printArray(const double array[], int size);
+	void countNumbers();
+}
