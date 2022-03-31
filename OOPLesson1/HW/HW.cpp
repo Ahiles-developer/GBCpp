1028db1d7fb4ae11929c862b5c96f47b158bb6d8
Ahiles <yakushev_v@rambler.ru>
2022-03-31 16:33:33 +0300

    Д3_1 Осталось закодить класс Stack

---
+++ OOPLesson1/HW/HW.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <cstdint>
+
+class Power {
+    int a = 5;
+    int b = 10;
+
+public:
+    void setNumbers(int numberOne, int numberTwo) {
+        a = numberOne;
+        b = numberTwo;
+    }
+
+    void calculate() {
+        int c = a * b;
+        std::cout << c << std::endl;
+    }
+};
+
+class RGBA {
+    std::uint8_t 
+        m_red   = 0, 
+        m_green = 0, 
+        m_blue  = 0, 
+        m_alpha = 255;
+
+public:
+    RGBA() {}; // Добавил т.к. конструктор по умолчанию отсутствовал, в связи с чем была ошибка в главном методе
+    RGBA(std::uint8_t m_red, std::uint8_t m_green, std::uint8_t m_blue, std::uint8_t m_alpha) {
+        this->m_red = m_red;
+        this->m_green = m_green;
+        this->m_blue = m_blue;
+        this->m_alpha = m_alpha;
+    }
+
+    void print() {
+        printf("%d %d %d %d", m_red, m_green, m_blue, m_alpha);
+    }
+};
+
+class Stack {
+    // код...
+
+public:
+    void reset(){}
+    void push(){}
+    void pop(){}
+    void print(){}
+};
+
+int main()
+{
+    Power p;
+    // p.setNumbers(5, 10);
+    p.calculate();
+
+    RGBA r;
+    r.print();
+
+    return 0;
+}
+
---
+++ OOPLesson1/OOPLesson1/OOPLesson1.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+
+class Cat {
+private:
+	// private-public 
+	int yearOfBirth;
+	std::string name;
+	// mutable - позволяет изменять значение там, где запрещено(const например)
+	mutable std::string color; // позволяет изменять константное значение
+
+public:
+	// initialization lists(спеисок инициализации - то что после двоеточия написано)
+	Cat() : yearOfBirth(2022) { // КОНСТРУКТОР ПО УМОЛЧАНИЮ(Если напишем другой конструктор, то конст. по умолчанию перестанет существовать)
+		color = "Peachy"; // here can be color randomizer
+		std::cout << "MEOW!" << std::endl;
+	}
+
+	Cat(std::string name, std::string color, int yearOfBirth=2021) { // в скобках можно устанавливать дефолтные параметры
+		this->name = name;
+		this->color = color;
+		this->yearOfBirth = yearOfBirth;
+	}
+	// copy constructor
+	Cat(Cat& c)
+		: name(c.name), color(c.color), yearOfBirth(c.yearOfBirth) {}
+
+
+	// destructors
+	~Cat() {
+		std::cout << "cat..." << std::endl;
+	}
+
+	// functions constructors
+	void voice() {
+		std::cout << name << " meow" << std::endl;
+	}
+
+	void move() {
+		std::cout << name << " walks on paws" << std::endl;
+	}
+
+	void setName(std::string n) { name = n; }
+
+	// const metods
+	int getAge() const { return 2022 - yearOfBirth; } // Константные функции изменять нельзя
+	std::string getName() const { return name; }
+	std::string getColor() const { return color; }
+	void info() {
+		printf("%s is %s and %d years of age\n", getName().c_str(), color.c_str(), getAge());
+	}
+};
+
+// links vs pointers
+void makeCatMove(Cat* c) {
+	(*c).move();
+	// c->move();
+}
+
+void makeCatMove(Cat& c) {
+	c.move();
+}
+
+
+int main(int argc, const char** argv) {
+	// classes vs objects, fields
+	Cat c0("Barsik", "White", 2017);
+//	c0.setAge(5);
+//	c0.setName("Barsik");
+//	c0.setColor("White");
+	c0.info();
+
+	Cat c1("Murzik", "Black", 2020);
+	c1.info();
+
+	Cat c2;
+	c2.setName("Kotik");
+	c2.info();
+	c2.info();
+	c2.info();
+
+	// Cat.voice(); // compile error
+	c0.voice();
+	c1.voice();
+	c0.move();
+	c1.move();
+	
+	// pointer to object
+	Cat* c2p = &c2;
+	c2p->setName("pointer"); // сокращенный синтаксис разыменования
+	c2.info();
+
+	Cat c4(c1); // клон кота c4 от c1
+	c4.setName("Clone");
+	c4.info();
+	c1.info();
+	
+	// this
+
+	return 0;
+}
