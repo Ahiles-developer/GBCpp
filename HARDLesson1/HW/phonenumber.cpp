9086ec0cbb4c89b59796cf9729581d4bb2217eff
Ahiles <yakushev_v@rambler.ru>
2022-05-20 17:40:51 +0300

    правки в дз

--- HARDLesson1/HW/phonenumber.cpp
+++ HARDLesson1/HW/phonenumber.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <optional>
+#include <tuple>
 
 struct PhoneNumber {
 	int codeCountry;
@@ -8,7 +9,23 @@ struct PhoneNumber {
 	std::optional<int> extensionNumber;
 };
 
-std::ostream& operator<<(std::ostream out, const PhoneNumber& pN) {
-	return out << "Phone number: +" << pN.codeCountry << "(" << pN.codeCity << ")"
-		<< pN.number << " " << pN.extensionNumber << "\n";
+std::ostream& operator<< (std::ostream& out, const PhoneNumber& p) {
+	out << "Phone number: +" << p.codeCountry << "(" << p.codeCity << ")" <<
+		p.number;
+	if (p.extensionNumber.has_value()) {
+		out << p.extensionNumber.value();
+	} else {
+		out << "";
+	}
+	return out;
+}
+
+bool operator<(const PhoneNumber& p1, const PhoneNumber& p2) {
+	return std::tie(p1.codeCountry, p1.codeCity, p1.number, p1.extensionNumber) <
+		   std::tie(p2.codeCountry, p2.codeCity, p2.number, p2.extensionNumber);
+}
+
+bool operator==(const PhoneNumber& p1, const PhoneNumber& p2) {
+	return std::tie(p1.codeCountry, p1.codeCity, p1.number, p1.extensionNumber) == 
+		   std::tie(p2.codeCountry, p2.codeCity, p2.number, p2.extensionNumber);
 }
