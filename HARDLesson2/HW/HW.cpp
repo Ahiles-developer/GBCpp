5f166c032a3b225621c5fa3794128ff79f102f63
Ahiles <yakushev_v@rambler.ru>
2022-05-25 15:04:40 +0300

    hw lesson 2

---
+++ HARDLesson2/HW/HW.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void mySwap(T& a, T& b) { 
+    std::cout << "swap(" << a << ", " << b << ")\n";
+    T t;
+    t = a;
+    a = b;
+    b = t;
+}
+
+template<typename T>
+void sortPointers(vector<int*> &v) {
+
+}
+
+int main()
+{
+    int a = 10, b = 5;
+    std::cout << a << " " << b << '\n';
+    mySwap(a, b);
+    std::cout << a << " " << b << '\n';
+
+
+    return 0;
+}
+
