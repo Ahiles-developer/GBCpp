5013c73bba12576cd455b02120091ef421ba43f2
Ahiles <yakushev_v@rambler.ru>
2022-06-02 15:39:52 +0300

    ДЗ

---
+++ HARDLesson4/HW/HW.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+
+void insert_sorted(std::vector<int> &v, int val) {
+    v.push_back(val);
+    std::sort(v.begin(), v.end());
+}
+
+template<typename T>
+T insert_sorted_any(T& v, int val) {
+    v.push_back(val);
+    std::sort(v.begin(), v.end());
+}
+
+int main()
+{
+    std::vector<int> v = { 1, 4, 7, 8, 2, 9 };
+    std::cout << "Don't sorted: ";
+    for (auto a : v) {
+        std::cout << a << " ";
+    }
+    std::cout << "\n";
+
+    insert_sorted(v, 3);
+    std::cout << "Add and sorted: ";
+    for (auto a : v) {
+        std::cout << a << " ";
+    }
+
+    std::list<int> l{1, 4, 7, 9,};
+    insert_sorted_any(l, 10);
+    for (auto a : l) {
+        std::cout << a << " ";
+    }
+    return 0;
+}
