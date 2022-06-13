d54bc842997bcafda00b1361f48962d2ddbce9dc
Ahiles <yakushev_v@rambler.ru>
2022-06-13 19:28:43 +0300

    HW

---
+++ HARDLesson5/HW/HW.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <set>
+using namespace std;
+
+template <typename T, typename Iterator>
+void uniqElements(const Iterator& begin, const Iterator& end) {
+	set<T> elements;
+	copy(begin, end, inserter(elements, elements.end()));
+	copy(elements.begin(), elements.end(), ostream_iterator<T>(cout, ", "));
+	cout << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	vector<string> str = { "one", "two", "three", "four", "one", "five", "five", "one" };
+	cout << "Список уникальных слов: ";
+	uniqElements<string>(str.begin(), str.end());
+	
+	vector<int> num = { 1, 2, 3, 4, 1, 5, 5, 1 };
+	cout << "Список уникальных цифр: ";
+	uniqElements<int>(num.begin(), num.end());
+
+    return 0;
+}
